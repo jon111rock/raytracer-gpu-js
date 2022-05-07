28d35d76a39d3227fa84172f1b03260daaf12ac6
薛猷騰 <uton@uton.local>
2022-05-08 00:45:30 +0800

    Refactor code

--- src/index.js
+++ src/index.js
@@ -2,20 +2,22 @@ import { GPU } from "./modules/gpu";
 
 let gpu = new GPU();
 
-const render = gpu.createKernel(
-  function () {
-    let tx = this.thread.x / 512;
-    let ty = this.thread.y / 512;
+const width = 512,
+  height = 512;
 
-    this.color(tx, ty, 0.25, 1);
-  },
-  {
-    output: [512, 512],
-    graphical: true,
-  }
-);
+//kernel
+const settings = {
+  output: [width, height],
+  graphical: true,
+};
+const render = gpu.createKernel(function (w, h) {
+  let tx = this.thread.x / w;
+  let ty = this.thread.y / h;
 
-render();
+  this.color(tx, ty, 0.25, 1);
+}, settings);
+
+render(width, height);
 
 const canvas = render.canvas;
 document.body.appendChild(canvas);
