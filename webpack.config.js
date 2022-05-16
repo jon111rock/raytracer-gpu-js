e6b6a88b89bda01fb759cda80bf748973a78b524
yu teng hsueh <jon111rock@gmail.com>
2022-05-16 19:04:41 +0800

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssEctractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/docs"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new MiniCssEctractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js||jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [MiniCssEctractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    open: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
